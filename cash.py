26aa622b89adea68723dfeb05dc482da1da562f3
Aarif <03aarif20@gmail.com>
2023-02-21 20:24:47 +0530

    Update cash.py

--- cash.py
+++ cash.py
@@ -8,7 +8,8 @@ def load(admin_list,st):
     five_hundred=st[3:6]
     two_hundred=st[6:9]
     hundred=st[9:12]
-    if int(two_thousand)<100 or int(two_thousand)>0 or int(five_hundred)<100 or int(five_hundred)>0 or int(two_hundred)<100 or int(two_hundred)>0 or int(hundred)<100 or int(hundred)<0:
+    am=int(st[12:])
+    if int(two_thousand)<=100 or int(two_thousand)>=0 or int(five_hundred)<=100 or int(five_hundred)>=0 or int(two_hundred)<=100 or int(two_hundred)>=0 or int(hundred)<=100 or int(hundred)<=0:
         l[0]=l[0]+int(two_thousand)
         l[1]=l[1]+int(five_hundred)
         l[2]=l[2]+int(two_hundred)
@@ -24,11 +25,16 @@ def load(admin_list,st):
             five_h=int(five_hundred) * 500
             two_h=int(two_hundred) * 200
             h=int(hundred) * 100
-            passd.balance=passd.balance+two_t+five_h+two_h+h
-            passd.money=l
-            print("Amount loaded successfully")
+            temp=two_t+five_h+two_h+h
+            if temp==am:
+                passd.balance=passd.balance+temp
+                passd.money=l
+                passd.history.append("Credited by admin of amount $"+str(am))
+                return "Amount of("+str(am)+") loaded successfully to the ATM"
+            else:
+                print("Entered amount and denomination doesn't match")
     else:
-        print("Enter the correct amount format")
+        print("Tray cannot accept more than 100 notes")
 def add(user_list,accno,pin):
     if int(accno) in user_list.keys():
         return "Error: That name already exists"
@@ -40,7 +46,7 @@ def bal_check(user_list,accno,pin):
         if int(pin)==card.pin:
             return "Account balance for acc no "+str(accno)+" is $"+str(card.balance)
         else:
-            return "Invalid Pin"+"for Account No : "+str(accno)
+            return "Invalid pin"+"for account no : "+str(accno)
     else:
         return "Account not found"
 def find_notes_combination(amount, denominations, notes_count, index=0, combination=None):
@@ -54,10 +60,10 @@ def find_notes_combination(amount, denominations, notes_count, index=0, combinat
     for i in range(notes_count[index]+1):
         if i * denominations[index] <= amount:
             next_combination = find_notes_combination(amount - i * denominations[index], denominations, notes_count, index+1, combination + [denominations[index]]*i)
-            if next_combination is not None and next_combination not in notes:
+            if next_combination is not None:
                 notes.append(next_combination + [denominations[index]]*i)
     if len(notes) == 0:
-        return
+        return 
     return min(notes,key=len)
 def withdraw(user_list,accno,pin,amount,admin_list):
     note=[100,200,500,2000]
@@ -66,16 +72,16 @@ def withdraw(user_list,accno,pin,amount,admin_list):
     if int(accno) in user_list.keys():
         card=user_list[int(accno)]
         if int(pin)==card.pin:
-            if int(amount) > sum([a * b for a, b in zip(note,l)]):
-                return "Sorry, the ATM does not have enough cash."
+            if int(amount) > passd.balance:
+                return "Sorry! the ATM does not have enough cash."
             elif int(amount)%100!=0:
-                return "Please!Enter the amount in multiples of hundred" 
+                return "Please! Enter the amount in multiples of hundred" 
             elif card.balance<int(amount):
                 return "Funds not available in your Account"
             else:
-                combination = find_notes_combination(int(amount),note,l)
+                combination = find_notes_combination(int(amount),note,l[::-1])
                 if combination is None:
-                    return "Sorry,ATM Cannot dispense the amount with aailable notes."
+                    return "Sorry! ATM cannot dispense the amount with available notes."
                 else:
                     ggg=0
                     hhh=0
@@ -90,31 +96,30 @@ def withdraw(user_list,accno,pin,amount,admin_list):
                             iii=iii+1
                         elif combination[i]==100:
                             jjj=jjj+1
-                    if ggg<=l[0]:
-                        l[0]=l[0]-ggg
-                    if hhh<=l[1]:
-                        l[1]=l[1]-hhh
-                    if iii<=l[2]:
-                        l[2]=l[2]-iii
-                    if hhh<=l[3]:
-                        l[3]=l[3]-hhh
+                    l[0]=l[0]-ggg
+                    l[1]=l[1]-hhh
+                    l[2]=l[2]-iii
+                    l[3]=l[3]-hhh
                     print("Cash $"+str(amount)+" withdrawed successfully with notes")
+                    print("----Notes---")
                     if ggg!=0:
-                        print("\n2000 --> "+str(ggg))
+                        print("2000 --> "+str(ggg))
                     if hhh!=0:
                         print("500 --> "+str(hhh))
                     if iii!=0:
                         print("200 --> "+str(iii))
                     if jjj!=0:
                         print("100 --> "+str(jjj))
+                    print("-----------")
                     card.balance=card.balance-int(amount)
-                    print("Account balance $"+str(card.balance))
                     card.history.append("Debited amount $"+str(amount)+" Closing Balance $"+str(card.balance))
                     passd.balance=passd.balance-int(amount)
                     passd.money=l
+                    passd.history.append("Debited by user of amount $"+str(amount))
+                    return "Account balance $"+str(card.balance)
 
         else:
-            return "Invalid Pin "+"for Account No : "+str(accno)
+            return "Invalid pin "+"for account No : "+str(accno)
     else:
         return "Account not found"
 def mini(user_list,accno,pin):
@@ -128,7 +133,7 @@ def mini(user_list,accno,pin):
                 for i in range(0,6):
                     print(card.history[i])
         else:
-             print("Invalid Pin for Account No : "+str(accno))
+             print("Invalid pin for account no : "+str(accno))
     else:
         print("Account not found")
 def depo(user_list,accno,pin,amount,admin_list,money_d):
@@ -149,13 +154,18 @@ def depo(user_list,accno,pin,amount,admin_list,money_d):
             five_h=int(five_hundred) * 500
             two_h=int(two_hundred) * 200
             h=int(hundred) * 100
-            passd.balance = passd.balance+two_t+five_h+two_h+h
-            card.balance=card.balance+int(amount)
-            card.history.append("Credited amount $"+str(amount)+" Closingbalance is $"+str(card.balance))
-            passd.money=l
-            return "Amount deposited successfully\nAccount balance : "+str(card.balance)
+            temp=two_t+five_h+two_h+h
+            if temp==int(amount):
+                passd.balance = passd.balance+temp
+                card.balance=card.balance+int(amount)
+                card.history.append("Credited amount $"+str(amount)+" Closingbalance is $"+str(card.balance))
+                passd.money=l
+                passd.history.append("Credited by user of amount $"+str(amount))
+                return "Amount deposited successfully\nAccount balance : "+str(card.balance)
+            else:
+                return "Entered amount and denomination doesn't match"
         else:
-            return "Invalid Pin "+"for Account No : "+str(accno)
+            return "Invalid pin "+"for account no : "+str(accno)
     else:
         return "Account not found"
     
@@ -168,14 +178,14 @@ def trans(user_list,accno,pin,amount,tranaccno):
             if int(tranaccno) in user_list.keys():
                 card1=user_list[int(tranaccno)]
                 card1.balance=card1.balance+int(amount)
-                card1.history.append("Credit transfer by Account No :"+str(accno)+" of amount $"+str(amount)+" Closingbalance is $"+str(card1.balance))
+                card1.history.append("Credit transfer by Account No : "+str(accno)+" of amount $"+str(amount)+" closing balance $"+str(card1.balance))
                 card.balance=card.balance-int(amount)
-                card.history.append("Transfered of amount $"+str(amount)+" to Accno "+str(tranaccno)+" Closing Balance $"+str(card.balance))
-                return "Transfer succesfull of amount "+str(amount)+"to Account no : "+str(tranaccno)+"\nYour account balance is"+str(card.balance)
+                card.history.append("Transfered of amount $"+str(amount)+" to Account no "+str(tranaccno)+" closing balance $"+str(card.balance))
+                return "Transfer succesfull of amount "+str(amount)+"to Account no : "+str(tranaccno)+"\nYour account balance is $"+str(card.balance)
             else:
-                return "No account found with userno : "+str(tranaccno)
+                return "No account found with no : "+str(tranaccno)
         else:
-            return "Invalid Pin"+"for Account No : "+str(accno)
+            return "Invalid pin"+"for account no : "+str(accno)
     else:
         return "Account not found"
 def pinchan(user_list,accno,pin,newpin):
@@ -183,9 +193,9 @@ def pinchan(user_list,accno,pin,newpin):
         card=user_list[int(accno)]
         if int(pin)==card.pin:
             card.pin=int(newpin)
-            return "Pin changed successfully for Account No : "+str(accno)
+            return "Pin changed successfully for account no : "+str(accno)
         else:
-            return "Invalid Pin"+"for Account No : "+str(accno)
+            return "Invalid pin"+"for account no : "+str(accno)
     else:
         return "Account not found"
 
